1815c2f2b50550d6e42bae62be98ea258c07f9b9
Kaustubh <kaustubhr2001@gmail.com>
2024-02-10 00:27:12 +0530

    Back to react native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {Button, StatusBar, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
-    </View>
-  )
-}
+      <StatusBar />
 
-export default App
+      <Text style={styles.button}> Press Me</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  status: {
+    backgroundColor: 'red',
+  },
+  button: {
+    backgroundColor: 'blue',
+  },
+});
